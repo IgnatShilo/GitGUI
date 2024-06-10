956ebb5875204703d9ae40da6bf8ff8fe54326b3
unknown <229199-30>
2024-06-10 16:07:25 +0300

    Revert "Версия 2.1"
    This reverts commit 598b38ebc58e980c1d9bc4c047f190dfa69a611d.
    
    # Conflicts:
    #	gui.cpp

--- gui.cpp
+++ gui.cpp
@@ -1,28 +1,8 @@
-﻿#include <iostream>
+﻿
 
-int main() {
-	setlocale(LC_ALL, "rus");
-	using namespace std;
-	double y, x, s, h, n, sum;
-	double a = 0.2, b = 0.8;
-	h = (b - a) / 10;
-	int k = 100;
+#include <iostream>
 
-
-	for (x = a; x <= b; x += h)
-	{
-		y = pow(3, x - 1);
-		s = 1;
-		sum = 1;
-		cout << "y(x)= " << y << " || ";
-
-		for (n = 1; n <= k; n++) {
-			s *= (x - 1)*log(3) / n;
-			sum += s;
-		}
-		cout << "s(x) = " << sum << endl;
-	}
-
+<<<<<<< HEAD
 <<<<<<< HEAD
 int main()
 {
@@ -32,6 +12,11 @@ int main()
 	std::cout << "Master's branch";
 =======
 >>>>>>> 598b38ebc58e980c1d9bc4c047f190dfa69a611d
+=======
+int main()
+{
+	std::cout << "Hello Ignat!\n";
+>>>>>>> parent of 598b38e (Версия 2.1)
 }
 
 
