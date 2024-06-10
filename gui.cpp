44d7c7339461352784bfefcafb0451b12913943f
sunshixd <167434443+sunshixd@users.noreply.github.com>
2024-06-10 16:34:04 +0300

    Update Second Branch From master's branch

--- gui.cpp
+++ gui.cpp
@@ -1,22 +1,14 @@
-﻿
-
 #include <iostream>
-
-<<<<<<< HEAD
-<<<<<<< HEAD
 int main()
 {
     std::cout << "Hello World!\n";
 	std::cout << "Hello Ignat!\n";
 	std::cout << "Hello for all!\n";
 	std::cout << "Master's branch";
-=======
->>>>>>> 598b38ebc58e980c1d9bc4c047f190dfa69a611d
-=======
+
 int main()
 {
 	std::cout << "Hello Ignat!\n";
->>>>>>> parent of 598b38e (Версия 2.1)
 }
 
 
